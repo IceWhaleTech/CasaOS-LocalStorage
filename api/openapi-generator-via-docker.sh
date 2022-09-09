177553b5541a03767ab5ac380a36484aea882f7a
Tiger Wang <tigerwang@outlook.com>
2022-09-09 12:14:37 -0400

    wip

---
+++ api/openapi-generator-via-docker.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+
+rm -rf target
+mkdir target
+docker run --rm --user "$(id -u):$(id -g)" -v "${PWD}":/local -e GO_POST_PROCESS_FILE="bash /local/api/openapi-generator-postprocess.sh" openapitools/openapi-generator-cli generate -i /local/api/openapi.yaml -g go-server -o /local/target -p=onlyInterfaces=true,outputAsLibrary=true,router=chi,featureCORS=true,sourceFolder=codegen --strict-spec true --enable-post-process-file
--- openapi-generator-via-docker.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash
-
-rm -rf target
-mkdir target
-docker run --rm --user "$(id -u):$(id -g)" -v "${PWD}":/local -e GO_POST_PROCESS_FILE="bash /local/openapi-generator-postprocess.sh" openapitools/openapi-generator-cli generate -i /local/openapi.yaml -g go-server -o /local/target -p=onlyInterfaces=true,outputAsLibrary=true,router=chi,featureCORS=true,sourceFolder=codegen --strict-spec true --enable-post-process-file
