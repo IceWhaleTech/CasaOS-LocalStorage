2fa1ed7de4e42f14b97add077526ace1d185a987
Tiger Wang <tigerwang@outlook.com>
2022-09-08 20:16:19 -0400

    wip

--- service/model/o_disk.go
+++ service/model/o_disk.go
@@ -1,18 +1,17 @@
-/*
- * @Author: LinkLeong link@icewhale.org
- * @Date: 2021-12-07 17:14:41
- * @LastEditors: LinkLeong
- * @LastEditTime: 2022-08-17 18:46:43
- * @FilePath: /CasaOS/service/model/o_disk.go
- * @Description:
- * @Website: https://www.casaos.io
- * Copyright (c) 2022 by icewhale, All Rights Reserved.
+/*@Author: LinkLeong link@icewhale.org
+ *@Date: 2021-12-07 17:14:41
+ *@LastEditors: LinkLeong
+ *@LastEditTime: 2022-08-17 18:46:43
+ *@FilePath: /CasaOS/service/model/o_disk.go
+ *@Description:
+ *@Website: https://www.casaos.io
+ *Copyright (c) 2022 by icewhale, All Rights Reserved.
  */
 package model
 
 // SerialAdvanced Technology Attachment (STAT)
 type SerialDisk struct {
-	Id         uint   `gorm:"column:id;primary_key" json:"id"`
+	ID         uint   `gorm:"column:id;primary_key" json:"id"`
 	UUID       string `json:"uuid"`
 	Path       string `json:"path"`
 	State      int    `json:"state"`
