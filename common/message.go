65c1d922e93b736d328181f693ee3ee082291325
Tiger Wang <tigerwang@outlook.com>
2022-11-08 23:06:18 -0500

    wip

--- common/message.go
+++ common/message.go
@@ -1,24 +1,58 @@
 package common
 
 import (
+	"fmt"
+
 	"github.com/IceWhaleTech/CasaOS-Common/utils"
 	"github.com/IceWhaleTech/CasaOS-LocalStorage/codegen/message_bus"
+	"github.com/pilebones/go-udev/netlink"
 )
 
 var (
-	SourceID = "local-storage"
+	// devtype -> action -> event
+	EventTypes map[string]map[string]message_bus.EventType
 
-	EventTypes = []message_bus.EventType{
-		{
-			SourceID: &SourceID,
-			Name:     utils.Ptr("local-storage:disk:added"),
-			PropertyTypeList: utils.Ptr(
-				[]message_bus.PropertyType{
-					{Name: utils.Ptr("path")},
-				},
-			),
-		},
-
-		// TODO - more events
-	}
+	EventPropertyNamePath string
 )
+
+func init() {
+	EventPropertyNamePath = fmt.Sprintf("%s:%s", ServiceName, "path")
+
+	for _, devtype := range []string{"disk", "partition"} {
+		for _, action := range []string{"add", "remove"} {
+			if EventTypes == nil {
+				EventTypes = make(map[string]map[string]message_bus.EventType)
+			}
+
+			if EventTypes[devtype] == nil {
+				EventTypes[devtype] = make(map[string]message_bus.EventType)
+			}
+
+			EventTypes[devtype][action] = message_bus.EventType{
+				SourceID: utils.Ptr(ServiceName),
+				Name:     utils.Ptr(fmt.Sprintf("%s:%s:%s", ServiceName, devtype, action)),
+				PropertyTypeList: &[]message_bus.PropertyType{
+					{Name: utils.Ptr(EventPropertyNamePath)},
+				},
+			}
+		}
+	}
+}
+
+func EventAdapter(e netlink.UEvent) *message_bus.Event {
+	eventType, ok := EventTypes[e.Env["DEVTYPE"]][string(e.Action)]
+	if !ok {
+		return nil
+	}
+
+	return &message_bus.Event{
+		SourceID: eventType.SourceID,
+		Name:     eventType.Name,
+		Properties: &[]message_bus.Property{
+			{
+				Name:  utils.Ptr(EventPropertyNamePath),
+				Value: utils.Ptr(e.Env["DEVNAME"]),
+			},
+		},
+	}
+}
