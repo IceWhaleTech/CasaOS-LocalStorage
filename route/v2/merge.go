e4dbe725200545d27c2fd11314dd21b059a17554
link <a624669980@163.com>
2023-01-03 17:11:12 +0800

    Fix merge err (#32)
    Signed-off-by: link <a624669980@163.com>

--- route/v2/merge.go
+++ route/v2/merge.go
@@ -2,6 +2,9 @@ package v2
 
 import (
 	"fmt"
+
+	"io/ioutil"
+
 	"net/http"
 	"os"
 	"strings"
@@ -21,28 +24,22 @@ import (
 var MessageMergerFSNotEnabled = "mergerfs is not enabled - either it is not enabled in configuration file; merge point is not empty before mounting; or mergerfs is not installed"
 
 func (s *LocalStorage) GetMerges(ctx echo.Context, params codegen.GetMergesParams) error {
-	// if strings.ToLower(config.ServerInfo.EnableMergerFS) != "true" {
-	// 	return ctx.JSON(http.StatusServiceUnavailable, codegen.ResponseServiceUnavailable{Message: &MessageMergerFSNotEnabled})
-	// }
-
-	if params.MountPoint != nil {
-
-		merges, err := service.MyService.LocalStorage().GetMerges(params.MountPoint)
-		if err != nil {
-			message := err.Error()
-			return ctx.JSON(http.StatusInternalServerError, codegen.BaseResponse{Message: &message})
-		}
-		data := make([]codegen.Merge, 0, len(merges))
-		for _, merge := range merges {
-			data = append(data, MergeAdapterOut(merge))
-		}
-		message := "ok"
-		return ctx.JSON(http.StatusOK, codegen.GetMergesResponseOK{Data: &data, Message: &message})
-	} else {
-		message := "ok"
-		data := make([]codegen.Merge, 0)
-		return ctx.JSON(http.StatusOK, codegen.GetMergesResponseOK{Data: &data, Message: &message})
+	if strings.ToLower(config.ServerInfo.EnableMergerFS) != "true" {
+		return ctx.JSON(http.StatusServiceUnavailable, codegen.ResponseServiceUnavailable{Message: &MessageMergerFSNotEnabled})
 	}
+
+	merges, err := service.MyService.LocalStorage().GetMerges(params.MountPoint)
+	if err != nil {
+		message := err.Error()
+		return ctx.JSON(http.StatusInternalServerError, codegen.BaseResponse{Message: &message})
+	}
+	data := make([]codegen.Merge, 0, len(merges))
+	for _, merge := range merges {
+		data = append(data, MergeAdapterOut(merge))
+	}
+	message := "ok"
+	return ctx.JSON(http.StatusOK, codegen.GetMergesResponseOK{Data: &data, Message: &message})
+
 }
 
 func (s *LocalStorage) SetMerge(ctx echo.Context) error {
@@ -157,6 +154,15 @@ func (s *LocalStorage) InitMerge(ctx echo.Context) error {
 
 	if strings.ToLower(config.ServerInfo.EnableMergerFS) != "true" {
 		if !file.CheckNotExist(m.MountPoint) {
+
+			dir, _ := ioutil.ReadDir(constants.DefaultFilePath)
+			if len(dir) > 0 {
+				message := "Please make sure the /var/lib/casaos/files directory is empty"
+				return ctx.JSON(http.StatusBadRequest, codegen.ResponseBadRequest{Message: &message})
+			}
+
+			file.RMDir(constants.DefaultFilePath)
+
 			err := os.Rename(m.MountPoint, constants.DefaultFilePath)
 			if err != nil {
 				fmt.Println(err)
